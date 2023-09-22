91912c71a601b3ea323a1b7481d30e80ec42522b
Shad-islam <shadislam48@gmail.com>
2023-09-22 23:49:26 +0600

    Add a condition to show 00 when the countdown is complete.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let countDownDate = new Date("Sep 20, 2023 00:00:00").getTime();
+let countDownDate = new Date("Sep 29, 2023 00:00:00").getTime();
 let x = setInterval(function () {
   let now = new Date().getTime();
   let distance = countDownDate - now;
@@ -8,8 +8,15 @@ let x = setInterval(function () {
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("days").innerHTML = days;
-  document.getElementById("hours").innerHTML = hours;
-  document.getElementById("minutes").innerHTML = minutes;
-  document.getElementById("seconds").innerHTML = seconds;
+  if (distance < 0) {
+    document.getElementById("days").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+  } else {
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+  }
 }, 1000);
