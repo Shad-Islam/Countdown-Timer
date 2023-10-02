ffc5c9483e39c9fb1e4266bee2eb6cee4d1fd8da
Shad-islam <shadislam48@gmail.com>
2023-10-02 23:25:53 +0600

    Change count down date

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let countDownDate = new Date("Sep 29, 2023 00:00:00").getTime();
+let countDownDate = new Date("Oct 29, 2023 00:00:00").getTime();
 let x = setInterval(function () {
   let now = new Date().getTime();
   let distance = countDownDate - now;
